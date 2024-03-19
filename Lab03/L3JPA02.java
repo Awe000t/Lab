ae66bca6d5d7cbc5d6a6e0fa58148a334662302e
Awe000t <161306259+Awe000t@users.noreply.github.com>
2024-03-19 11:36:24 +0800

    Add files via upload

---
+++ Lab03/L3JPA02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JPA02 {
+    public static void main(String[] args) {
+        int count=0;
+        for(int i=1;i<=3;i++){
+            for(int t=1;t<=9;t++){
+                count+=1;
+            }
+        }
+        System.out.println("count = "+count);
+
+    }
+}
