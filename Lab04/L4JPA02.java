53711565e82315d7674b2ae5926da3376195e8d3
Awe000t <161306259+Awe000t@users.noreply.github.com>
2024-03-20 15:13:36 +0800

    Add files via upload

---
+++ Lab04/L4JPA02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class L4JPA02 {
+    public static void main(String[] args) {
+        double s=0,g;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("請輸入學生人數:");
+        int p = sc.nextInt();
+        for(int i=1;i<=p;i++){
+            System.out.printf("第%d個學生的成績:",i);
+            g = sc.nextDouble();
+            s+=g;
+        }
+        System.out.print("人數:"+p);
+        System.out.print("人數:"+s);
+        System.out.print("人數:"+(float)s/p);
+
+    }
+    
+}
