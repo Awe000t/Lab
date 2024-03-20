908dbfa769ec29113f9cb4c6a3af8bc4e745e4e7
Awe000t <161306259+Awe000t@users.noreply.github.com>
2024-03-20 15:14:29 +0800

    Update and rename JPA01.java to L4JPA01.java

--- Lab04/L4JPA01.java
+++ Lab04/L4JPA01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JPA01{
+public class L4JPA01{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i,n,s=0;
