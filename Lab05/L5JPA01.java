a61408882d780a9796b03479b1de1de5d38f6b8d
Awe000t <161306259+Awe000t@users.noreply.github.com>
2024-04-02 10:36:43 +0800

    Add files via upload

---
+++ Lab05/L5JPA01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class L5JPA01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Input n (0<=n<=16):");
+            int n = sc.nextInt();
+            System.out.printf("%d的乘階 = %d\n",n,fac(n));
+            if(n==999){
+                break;
+            }
+        }
+        sc.close();
+    }
+    static int fac(int a){
+        if(a==0){
+            return 1;
+        }else{
+            return a*fac(a-1);
+        }
+    }
+}
